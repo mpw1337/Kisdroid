6e51b7dbc29901b2ac4f4e09e3bb3de9b35e916c
mpw1337 <markus.wersig@googlemail.com>
2012-03-08 16:39:11 +0100

    Erste Anpassungen

--- Kisdroid/src/de/mpw/kisdroid/KisdroidActivity.java
+++ Kisdroid/src/de/mpw/kisdroid/KisdroidActivity.java
@@ -2,12 +2,33 @@ package de.mpw.kisdroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class KisdroidActivity extends Activity {
+	public KismetClient client;
+	public String SERVER;
+	public int PORT;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void onClick(final View view){
+    	switch (view.getId()) {
+		case R.id.sf_start:
+			SERVER= "192.168.2.11";
+			PORT = 2501;
+			client = new KismetClient(SERVER,PORT);
+			client.start();
+			break;
+		case R.id.sf_stop:
+			client.stopClient();
+			break;
+
+		default:
+			break;
+		}
     }
 }
---
+++ Kisdroid/src/de/mpw/kisdroid/KismetClient.java
@@ -0,0 +1,58 @@
+package de.mpw.kisdroid;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import android.os.Handler;
+
+public class KismetClient extends Thread {
+	
+	String mServer;
+	int mPort;
+	Boolean connected;
+	Handler msgHandler;
+	Socket mSocket;
+	BufferedReader in;
+	PrintWriter out;
+	volatile boolean running = true;
+	
+	
+	public KismetClient(String server, int port){
+		this.mPort = port;
+		this.mServer = server;
+		this.connected = false;
+		
+	}
+	
+	public void stopClient(){
+		running = false;
+	}
+	public void start(){
+	while(running){
+		
+		try {
+			InetAddress  adress = InetAddress.getByName(mServer);
+			mSocket = new Socket(adress,mPort);
+			out = new PrintWriter(mSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+			out.println("!1 ENABLE STATUS *");
+			out.println("!2 ENABLE SSID *");
+			out.println("!3 ENABLE TIME");
+			String fromServer = "";
+			while ((fromServer  = in.readLine()) != null /* && connected*/) {
+			//fromServer = in.readLine();
+				System.out.println(fromServer);
+			}
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+	}
+	}
+
+}
