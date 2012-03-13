a9a0fb02063df7f50c80b804defa14563e261715
mpw1337 <markus.wersig@googlemail.com>
2012-03-13 15:46:27 +0100

    Protocoll SSID implementiert.

---
+++ Kisdroid/src/de/mpw/kisdroid/protocols/Ssid.java
@@ -0,0 +1,36 @@
+package de.mpw.kisdroid.protocols;
+
+public class Ssid implements Protocols {
+	
+	private String raw;
+	private String[] mArray;
+	private String mSSID;
+	private String mMAC;
+	private static final String identifier = "*SSID";
+	
+	public Ssid(String string){
+		this.raw = string;
+		mArray = raw.split(":");
+		mMAC = mArray[0];
+		mSSID = mArray[1];
+	}
+
+	public String getRaw() {
+		// TODO Auto-generated method stub
+		return raw;
+	}
+	
+	public String getSsid(){
+		return mSSID;
+	}
+	
+	public String getMac(){
+		return mMAC;
+	}
+
+	public static String getIdentifier() {
+		// TODO Auto-generated method stub
+		return identifier;
+	}
+
+}
