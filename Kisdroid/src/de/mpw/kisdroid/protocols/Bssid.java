b635ee08aed10743d4fa1f2199d23462034877b6
mpw1337 <markus.wersig@googlemail.com>
2012-03-15 16:32:19 +0100

    Erweitertes Netzwerk Protokoll eingefügt, dass mehr Informationen über die Netzwerke liefert.

---
+++ Kisdroid/src/de/mpw/kisdroid/protocols/Bssid.java
@@ -0,0 +1,27 @@
+package de.mpw.kisdroid.protocols;
+
+/*
+ * bssid,type,llcpackets,datapackets,cryptpackets,manuf,channel,firsttime,lasttime,atype,rangeip,netmaskip,gatewayip,gpsfixed,minlat,minlon,minalt,minspd,maxlat,maxlon,maxalt,maxspd,signal_dbm,noise_dbm,minsignal_dbm,minnoise_dbm,maxsignal_dbm,maxnoise_dbm,signal_rssi,noise_rssi,minsignal_rssi,minnoise_rssi,maxsignal_rssi,maxnoise_rssi,bestlat,bestlon,bestalt,agglat,agglon,aggalt,aggpoints,datasize,turbocellnid,turbocellmode,turbocellsat,carrierset,maxseenrate,encodingset,decrypted,dupeivpackets,bsstimestamp,cdpdevice,cdpport,fragments,retries,newpackets,freqmhz,datacryptset
+ */
+/*CAPABILITY: BSSID 
+ * bssid,type,llcpackets,datapackets,cryptpackets,
+ * manuf,channel,firsttime,lasttime,atype,rangeip,
+ * netmaskip,gatewayip,gpsfixed,minlat,minlon,minalt,
+ * minspd,maxlat,maxlon,maxalt,maxspd,signal_dbm,noise_dbm,
+ * minsignal_dbm,minnoise_dbm,maxsignal_dbm,maxnoise_dbm,
+ * signal_rssi,noise_rssi,minsignal_rssi,minnoise_rssi,maxsignal_rssi,
+ * maxnoise_rssi,bestlat,bestlon,bestalt,agglat,agglon,aggalt,
+ * aggpoints,datasize,turbocellnid,turbocellmode,turbocellsat,
+ * carrierset,maxseenrate,encodingset,decrypted,dupeivpackets,bsstimestamp,
+ * cdpdevice,cdpport,fragments,retries,newpackets,freqmhz,datacryptset
+ */
+
+
+public class Bssid implements Protocols {
+
+	public String getRaw() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
