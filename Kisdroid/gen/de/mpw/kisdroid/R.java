9fe41a3ac02cf16babf85ea710dc0c4bfeac9cba
mpw1337 <markus.wersig@googlemail.com>
2012-03-20 15:19:38 +0100

    Hinzufügen einer Battery Seite die informationen über die Battery bereitstellt.

--- Kisdroid/gen/de/mpw/kisdroid/R.java
+++ Kisdroid/gen/de/mpw/kisdroid/R.java
@@ -10,36 +10,47 @@ package de.mpw.kisdroid;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int bat_padding=0x7f050000;
+        public static final int ueberschrift=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int opt_einstellungen=0x7f070004;
-        public static final int opt_start_stop=0x7f070005;
-        public static final int pf_Server=0x7f070003;
-        public static final int tv_Networks=0x7f070000;
-        public static final int tv_mac=0x7f070001;
-        public static final int tv_strength=0x7f070002;
+        public static final int opt_einstellungen=0x7f090006;
+        public static final int opt_start_stop=0x7f090007;
+        public static final int pf_Server=0x7f090005;
+        public static final int progressBar1=0x7f090001;
+        public static final int textView1=0x7f090000;
+        public static final int tv_Networks=0x7f090002;
+        public static final int tv_mac=0x7f090003;
+        public static final int tv_strength=0x7f090004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int battery=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int hauptmenue=0x7f060000;
+        public static final int hauptmenue=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int gps_3df=0x7f05000b;
-        public static final int gps_kf=0x7f05000a;
-        public static final int hello=0x7f050000;
-        public static final int opt_start_stop=0x7f050009;
-        public static final int pf_Server=0x7f050005;
-        public static final int pf_einstellungen=0x7f050008;
-        public static final int pf_host=0x7f050006;
-        public static final int pf_port=0x7f050007;
-        public static final int sf_start=0x7f050002;
-        public static final int sf_stop=0x7f050003;
-        public static final int toast_fehler=0x7f050004;
+        public static final int app_name=0x7f060001;
+        public static final int gps_3df=0x7f06000b;
+        public static final int gps_kf=0x7f06000a;
+        public static final int hello=0x7f060000;
+        public static final int opt_start_stop=0x7f060009;
+        public static final int pf_Server=0x7f060005;
+        public static final int pf_einstellungen=0x7f060008;
+        public static final int pf_host=0x7f060006;
+        public static final int pf_port=0x7f060007;
+        public static final int sf_start=0x7f060002;
+        public static final int sf_stop=0x7f060003;
+        public static final int toast_fehler=0x7f060004;
+        public static final int tx_bat_Ueberschrift=0x7f06000c;
+    }
+    public static final class style {
+        public static final int ueberschrift=0x7f070000;
     }
     public static final class xml {
         public static final int einstellungen=0x7f040000;
