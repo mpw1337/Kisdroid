af3a2a8c302f7d137ad9332803442c7322bdf343
mpw1337 <markus.wersig@googlemail.com>
2012-03-23 12:09:46 +0100

    Capability String hinzugefügt.

--- Kisdroid/src/de/mpw/kisdroid/protocols/Bssid.java
+++ Kisdroid/src/de/mpw/kisdroid/protocols/Bssid.java
@@ -78,6 +78,18 @@ package de.mpw.kisdroid.protocols;
  */
 public class Bssid implements Protocols {
 
+	public static final String CAPABILITY = "BSSID bssid,type,llcpackets,datapackets,cryptpackets,manuf,channel,"
+			+ "firsttime,lasttime,atype,rangeip,netmaskip,gatewayip,"
+			+ "gpsfixed,minlat,minlon,minalt,minspd,"
+			+ "maxlat,maxlon,maxalt,maxspd,"
+			+ "signal_dbm,noise_dbm,minsignal_dbm,minnoise_dbm,"
+			+ "maxsignal_dbm,maxnoise_dbm,signal_rssi,noise_rssi,"
+			+ "minsignal_rssi,minnoise_rssi,maxsignal_rssi, maxnoise_rssi,"
+			+ "bestlat,bestlon,bestalt,agglat,agglon,aggalt,aggpoints,"
+			+ "datasize,turbocellnid,turbocellmode,turbocellsat,carrierset,"
+			+ "maxseenrate,encodingset,decrypted,dupeivpackets,bsstimestamp,"			
+			+ "cdpdevice,cdpport,fragments,retries,newpackets,freqmhz,datacryptset";
+
 	private GPS mMinGps;
 	private GPS mMaxGps;
 	private GPS mBestGps;
