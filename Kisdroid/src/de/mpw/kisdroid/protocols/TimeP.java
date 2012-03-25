0f7d09f71c4de6d843e271f5e7b5d9ad0eeef85d
mpw1337 <markus.wersig@googlemail.com>
2012-03-25 18:02:46 +0200

    Anpassung der Zeit.

--- Kisdroid/src/de/mpw/kisdroid/protocols/TimeP.java
+++ Kisdroid/src/de/mpw/kisdroid/protocols/TimeP.java
@@ -1,27 +1,33 @@
 package de.mpw.kisdroid.protocols;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class TimeP implements Protocols {
-	
+
 	public static final String IDENTIFIER = "*TIME";
 	public static String EXTRA_TIME = "TIME";
 	public static String CAPABILITY = "!%n ENABLE BATTERY percentage,charging,ac,remaining";
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.getDefault());
 	private String raw;
-	
+
 	private Integer time;
 
-
-	public TimeP(String string){
+	public TimeP(String string) {
 		this.raw = string;
 		String[] temp = raw.split(":", 2);
 		// mArray = temp[1].indexOf("\x01", start)
 		temp[1] = temp[1].trim();
 		time = Integer.parseInt(temp[1]);
 	}
-	
-	public String getTime(){
-		return time.toString();
+
+	public String getTime() {
+		Date d = new Date(time.longValue() * 1000);
+		return sdf.format(d);
+		// return time.toString();
 	}
+
 	public String getRaw() {
 		return raw;
 	}
