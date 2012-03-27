dceb69fc81486da4e2c588531e546079c1695b74
mpw1337 <markus.wersig@googlemail.com>
2012-03-27 11:42:56 +0200

    Helper Object für Ip Adressen

---
+++ Kisdroid/src/de/mpw/kisdroid/helper/Ip.java
@@ -0,0 +1,29 @@
+package de.mpw.kisdroid.helper;
+
+/**
+ * @author Markus
+ * 
+ */
+public class Ip {
+
+	Integer[] mAdresse = new Integer[4];
+
+	/**
+	 * Dem Construktor wird ein String der Form "144.123.0.3" übergeben.
+	 */
+	public Ip(String adresse) {
+		String[] mTemp = adresse.split(".");
+		//String in die einzelnen Teile zerlegen
+		for (int i = 0; i < mTemp.length; i++) {
+			try {
+				mAdresse[i] = Integer.valueOf(mTemp[i]);
+			} catch (NumberFormatException e) {
+				mAdresse[i] = 0;
+			}
+		}
+	}
+	public Integer[] getIP(){
+		return mAdresse;
+	}
+
+}
