faa6f15f4407b8c309b89f6d6e88f7044757c7fc
mpw1337 <markus.wersig@googlemail.com>
2012-03-30 16:54:17 +0200

    Ignore

--- Kisdroid/gen/de/mpw/kisdroid/R.java
+++ Kisdroid/gen/de/mpw/kisdroid/R.java
@@ -41,6 +41,7 @@ public final class R {
         public static final int battery=0x7f030000;
         public static final int main=0x7f030001;
         public static final int map=0x7f030002;
+        public static final int tablayout=0x7f030003;
     }
     public static final class menu {
         public static final int hauptmenue=0x7f090000;
